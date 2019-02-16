4c6d7b1e5eef04254bb0a31db1cd3d0ed3778638
Chase Buhler <cmbuhler@ualberta.ca>
2019-02-15 21:14:03 -0700

    Updates

---
+++ precache-manifest.bfec2c28b1fd31934819e15ca0ae525d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c4cfa3beddf0241a8a8",
+    "url": "/react-weather-app/static/js/runtime~main.4c4cfa3b.js"
+  },
+  {
+    "revision": "44abb5974e53f66b3c18",
+    "url": "/react-weather-app/static/js/main.44abb597.chunk.js"
+  },
+  {
+    "revision": "154498e67551e7860416",
+    "url": "/react-weather-app/static/js/2.154498e6.chunk.js"
+  },
+  {
+    "revision": "44abb5974e53f66b3c18",
+    "url": "/react-weather-app/static/css/main.744500a2.chunk.css"
+  },
+  {
+    "revision": "a97aa15e266d1429fd362ddff76ba9a5",
+    "url": "/react-weather-app/index.html"
+  }
+];
